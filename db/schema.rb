1eb8075755404008bc04224d0941e5d388a42e65
Andrew <goldsabre@gmail.com>
2016-11-18 17:42:05 -0700

    Generated a post scaffold as this is a timed practice.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161119003935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "topic"
+    t.string   "title"
+    t.text     "post"
+    t.date     "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
