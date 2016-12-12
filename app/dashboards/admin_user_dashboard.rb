823568ba2a805262de2571a9e925b872faf7855e
Andrew <goldsabre@gmail.com>
2016-12-12 15:00:07 -0700

    Fixed admin user access, assigned posts to users

--- app/dashboards/admin_user_dashboard.rb
+++ app/dashboards/admin_user_dashboard.rb
@@ -37,8 +37,6 @@ class AdminUserDashboard < Administrate::BaseDashboard
     :id,
     :email,
     :password,
-    :encrypted_password,
-    :reset_password_token,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
@@ -70,15 +68,6 @@ class AdminUserDashboard < Administrate::BaseDashboard
   FORM_ATTRIBUTES = [
     :email,
     :password,
-    :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
-    :remember_created_at,
-    :sign_in_count,
-    :current_sign_in_at,
-    :last_sign_in_at,
-    :current_sign_in_ip,
-    :last_sign_in_ip,
     :first_name,
     :last_name,
     :string,
